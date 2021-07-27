475c7b7c68356acbff2739b3c32cb5fcd3df81f3
dhythm <yuta.okada.20@gmail.com>
2021-07-27 17:49:34 +0900

    feat: use useHover from react-gui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useHover } from "react-gui/use-hover";
 import { StyledView } from "./components/View";
 
 const App: React.FunctionComponent = () => {
+  const [hovered, onHoverChange] = React.useState(false);
+  const ref = useHover({ onHoverChange });
   return (
     <div className="App">
       <StyledView>Hello, World!</StyledView>
+      <div
+        ref={ref}
+        style={{
+          width: 200,
+          height: 200,
+          margin: "50px auto",
+          border: "1px solid black",
+          backgroundColor: hovered ? "orange" : null,
+        }}
+      />
     </div>
   );
 };
